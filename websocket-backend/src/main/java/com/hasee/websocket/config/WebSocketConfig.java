4f1fb110f84e374aef45aa727b5901ba2604539a
haseeAmarathunga <haseeamarathunga@gmail.com>
2022-10-04 10:47:57 +0530

    Update websocket configs and UI/UX

--- websocket-backend/src/main/java/com/hasee/websocket/config/WebSocketConfig.java
+++ websocket-backend/src/main/java/com/hasee/websocket/config/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,5 +20,13 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app")
                 .enableSimpleBroker("/message", "/passport");
+    }
+
+    @Override
+    public void configureWebSocketTransport( WebSocketTransportRegistration registration )
+    {
+        registration.setMessageSizeLimit( 300000 * 50 ); // default : 64 * 1024
+        registration.setSendTimeLimit( 30 * 10000 ); // default : 10 * 10000
+        registration.setSendBufferSizeLimit( 3 * 512 * 1024 ); // default : 512 * 1024
     }
 }
--- websocket-frontend/src/environments/environment.prod.ts
+++ websocket-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  app_url: 'http://localhost:8082/socket'
 };
